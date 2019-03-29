0978d789dfe52443226a339bfdc6281ff4d0b870
jiajie zhao <jjzgood@126.com>
2019-03-30 06:53:41 +0800

    add MB/s

--- net_in_out.py
+++ net_in_out.py
@@ -2,13 +2,6 @@ import sys
 import time
 
 
-# Real-time monitoring of interface 
-if len(sys.argv) > 1:
-    interface = sys.argv[1]
-else:
-    interface = 'eth0'
-print('Interface:{}'.format(interface))
-
 def traffic():
     # network file
     with open('/proc/net/dev') as f:
@@ -16,7 +9,17 @@ def traffic():
         for line in data:
             if interface in line:
                 stats.append([float(line.split()[1]),float(line.split()[9])])
+            else:
+                print('interface name:{} error.'.format(interface))
+                sys.exit(1)
 
+
+# Real-time monitoring of interface 
+if len(sys.argv) > 1:
+    interface = sys.argv[1]
+else:
+    interface = 'eth0'
+print('Interface:{}'.format(interface))
 while True:
     stats = []
     traffic()
@@ -24,5 +27,5 @@ while True:
     traffic()
     IN = round((stats[1][0] - stats[0][0])/1024/1024,3)
     OUT = round((stats[1][1] - stats[0][1])/1024/1024,3)
-    print("NET_IN:{}\t\tNET_OUT:{}".format(IN,OUT))
-    stats.clear()
+    print("NET_IN:{}MB/s\t\tNET_OUT:{}MB/s".format(IN,OUT))
+    stats.pop(0)
